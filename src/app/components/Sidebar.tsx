4f4ee91dfbe3091749f3ae88f57d8e6fd8925123
Son Le <son.le@expereo.com>
2023-03-15 12:18:55 +0700

    profile line

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
   HiHome,
   HiOutlineMail,
   HiOutlineUser,
+  HiOutlineDotsHorizontal,
 } from "react-icons/hi";
 
 export default function Sidebar() {
@@ -35,8 +36,24 @@ export default function Sidebar() {
         <SidebarMenuItem Icon={HiOutlineUser} text="Profile" />
         <SidebarMenuItem Icon={HiOutlineDotsCircleHorizontal} text="More" />
       </div>
+
       {/* Button */}
+      <button>Tweet</button>
+
       {/* Profile */}
+      <div className="">
+        <Image
+          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt="Profile picture"
+          width={80}
+          height={80}
+        />
+        <div className="">
+          <h4>Amanda Swayze</h4>
+          <p>@AmSwayze</p>
+          <HiOutlineDotsHorizontal className="h-5" />
+        </div>
+      </div>
     </div>
   );
 }
