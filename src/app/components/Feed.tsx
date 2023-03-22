c55e0d09cc9f0336037fc9961461921a3df2fdee
Son Le <son.le@expereo.com>
2023-03-22 23:30:41 +0700

    feed component

---
+++ src/app/components/Feed.tsx
@@ -0,0 +1,14 @@
+import { HiOutlineSparkles } from "react-icons/hi";
+
+export default function Feed() {
+  return (
+    <div className="max-w-xl flex-grow border-l border-r border-gray-200 sm:ml-[73px] xl:ml-[370px] xl:min-w-[576px]">
+      <div className="sticky top-0 z-50 flex border-b border-gray-200 bg-white py-2 px-3">
+        <h2 className="cursor-pointer text-lg font-bold sm:text-xl">Home</h2>
+        <div className="hoverEffect ml-auto flex h-9 w-9 items-center justify-center px-0">
+          <HiOutlineSparkles className="h-5" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Head from "next/head";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function Home() {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
         {/* Widget */}
       </main>
     </div>
